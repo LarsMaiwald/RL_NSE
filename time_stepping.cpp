93e30c05ce41d62032b77a637adc3d06dd215cad
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-04-28 15:12:16 +0200

    further implementation of the time stepping

--- time_stepping.cpp
+++ time_stepping.cpp
@@ -14,10 +14,22 @@ float array_max(float **a, int i_max, int j_max){
     return array_max;
 }
 
-float time_stepping(float **u, float **v, float tau, int Re, float dx, float dy, int i_max, int j_max){
+float time_step(float **u, float **v, float tau, int Re, float dx, float dy, int i_max, int j_max){
     float v1 = Re/(2*(1/(dx*dx) + 1/(dy*dy)));
     float v2 = dx/abs(array_max(u, i_max, j_max));
     float v3 = dy/abs(array_max(v, i_max, j_max));
     float dt = tau*min({v1, v2, v3});
     return dt;
 }
+
+//void iterate(float **u, float **v, float tau, int Re, float dx, float dy, int i_max, int j_max, int num_iter){
+//    for(int k = 0; k < num_iter; k++){
+//        float dt = time_step(u, v, tau, Re, dx, dy, i_max, j_max);
+//        for(int i = 0; i < i_max; i++){
+//            for(int j = 0; j < j_max; j++){
+//                u[i][j] = F[i][j] - dt*dpdx[i][j];
+//                v[i][j] = G[i][j] - dt*dpdy[i][j];
+//            }
+//        }
+//    }
+//}
