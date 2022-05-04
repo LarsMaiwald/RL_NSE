082014bd77eadeb4551de6266f0164c9312d4ce8
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-05-04 17:26:49 +0200

    grid initialization with example function

--- initialization.cpp
+++ initialization.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
+#include <cmath>
+#include "Grid.h"
 using namespace std;
+
+// Initialization of grid (a x b) values with predefined example function
+void grid_init(Grid u, int a, int b)
+{
+    for (int i = 0; i < u.i_max + 2; i++) {
+        for (int j = 0; j < u.j_max + 2; j++) {
+            u.grid[i][j] = cos(i*a/(u.i_max+2))*sin(j*b/(u.j_max+2));
+        }
+    }
+}
