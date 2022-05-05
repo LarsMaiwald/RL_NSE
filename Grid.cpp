df9324033d4e74c9a83fcd7ae031bc03eaaa83b5
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-05-05 11:34:50 +0200

    fixed small error

--- Grid.cpp
+++ Grid.cpp
@@ -4,36 +4,36 @@ using namespace std;
 
 Grid::Grid(int i, int j)
 {
-  i_max = i;
-  j_max = j;
+    i_max = i;
+    j_max = j;
 
-  float** u = new float*[i_max + 2];
-  for (int i = 0; i < i_max + 2; i++) {
-      u[i] = new float[j_max + 2];
-  }
-  for (int i = 0; i < i_max + 2; i++) {
-      for (int j = 0; j < j_max + 2; j++) {
-          u[i][j] = 0;
-      }
-  grid = u;
-  }
+    float** u = new float*[i_max + 2];
+    for (int i = 0; i < i_max + 2; i++) {
+        u[i] = new float[j_max + 2];
+    }
+    for (int i = 0; i < i_max + 2; i++) {
+        for (int j = 0; j < j_max + 2; j++) {
+            u[i][j] = 0;
+        }
+    }
+    grid = u;
 }
 
 Grid::~Grid()
 {
-  for(int i=0; i < i_max + 2; i++)
-  {
-      delete [] grid[i];
-  }
-  delete [] grid;
+    for(int i=0; i < i_max + 2; i++)
+    {
+        delete [] grid[i];
+    }
+    delete [] grid;
 }
 
 void Grid::print()
 {
-  for (int i = 0; i < i_max + 2; i++) {
-      for (int j = 0; j < j_max + 2; j++) {
-          cout << grid[i][j] << " ";
-      }
-      cout << "\n";
-}
+    for (int i = 0; i < i_max + 2; i++) {
+        for (int j = 0; j < j_max + 2; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
