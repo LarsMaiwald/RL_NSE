cda6ed011d2526e3d0d275038c0a1bd89b3b3d10
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-05-06 13:32:26 +0200

    loading parameter file in python

---
+++ load_cfg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 12:55:45 2022
+
+@author: Lars Maiwald
+"""
+
+import io, libconf
+
+def load_cfg(path):
+    with io.open(path) as f:
+        cfg = libconf.load(f)
+    return cfg
