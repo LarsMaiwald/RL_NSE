072643ab165677974d41e0f47b7bcb447bdc37be
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-05-09 09:24:47 +0200

    deleting old code which got replaced by the Grid class

--- memory_management.cpp
+++ memory_management.cpp
@@ -1,26 +1,3 @@
 #include <iostream>
 using namespace std;
 
-float ** grid(int i_max, int j_max)
-{
-  // Array initialization (shouldn't this be in "initialization.cpp"?)
-  float** u = new float*[i_max];
-  for (int i = 0; i < i_max; i++) {
-      u[i] = new float[j_max];
-  }
-  for (int i = 0; i < i_max; i++) {
-      for (int j = 0; j < j_max; j++) {
-          u[i][j] = 0;
-      }
-  }
-  return u;
-}
-
-void delete_grid(float ** u, int i_max, int j_max)
-{
-  for(int i=0; i < i_max; i++)
-  {
-      delete [] u[i];
-  }
-  delete [] u;
-}
--- memory_management.h
+++ memory_management.h
@@ -1,8 +1,4 @@
 #ifndef MEMORY_MANAGEMENT_H
 #define MEMORY_MANAGEMENT_H
 
-float ** grid(int i_max, int j_max);
-
-void delete_grid(float ** u, int i_max, int j_max);
-
 #endif // MEMORY_MANAGEMENT_H
