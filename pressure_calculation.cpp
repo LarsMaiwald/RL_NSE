5f416f09b8e633adcc99012e40a0e5421a41d48e
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-05-16 09:54:52 +0200

    adding calculation of pressure derivatives

--- pressure_calculation.cpp
+++ pressure_calculation.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include "Grid.h"
 using namespace std;
+
+void pressure_deriv(Grid &p, Grid &dpdx, Grid &dpdy, float dx, float dy){
+    for(int i = 0; i < p.i_max + p.i_g; i++){ // is the range correct? --> no
+        for(int j = 0; j < p.j_max + p.j_g; j++){ // is the range correct? --> no
+            dpdx.grid[i][j] = (p.grid[i+1][j] - p.grid[i][j])/dx;
+            dpdy.grid[i][j] = (p.grid[i][j+1] - p.grid[i][j])/dy;
+        }
+    }
+}
