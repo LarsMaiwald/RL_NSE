587836e7ba50b0c1987314ff89cc46a6cd8332e8
Rudi <k.r.siebert@icloud.com>
2022-06-09 16:09:26 +0200

    cmakelists and READ_ME

---
+++ testing_derivatives/READ_ME.txt
@@ -0,0 +1,3 @@
+
+	- I thought putting the extra task into separate directories would make things easier.
+	- The plan is to save all derivatives as .csv files in outputs and then plot them against simpy results using python.
--- testing_derivatives/main.cpp
+++ testing_derivatives/main.cpp
@@ -13,10 +13,10 @@ int main()
     // Initializatio
 
     // Creating all grid
-    Grid x(i_max, j_max, 1,);
-    Grid u(i_max, j_max, 1, 2);
-    Grid d2udx2(i_max, j_max, 1, 2);
-    Grid d2udy2(i_max, j_max, 1, 2);
+    Grid x(i_max, j_max, 2, 2);
+    Grid u(i_max, j_max, 2, 2);
+    Grid d2udx2(i_max, j_max, 2, 2);
+    Grid d2udy2(i_max, j_max, 2, 2);
     Grid du2dx(i_max, j_max, 1, 2);
     Grid duvdy(i_max, j_max, 1, 2);
     Grid v(i_max, j_max, 2, 1);
@@ -37,15 +37,15 @@ int main()
     derivative_stencils(u, v, d2udx2, d2udy2, du2dx, duvdy, d2vdx2, d2vdy2, duvdx, dv2dy, dx, dy, gamma);
     // Final output
 //    cout << "Outputting grids \n";
-    grid2file(u, "../RL_NSE/outputs/u_final.csv");
-    grid2file(d2udx2, "../RL_NSE/outputs/d2udx2_final.csv");
-    grid2file(d2udy2, "../RL_NSE/outputs/d2udy2_final.csv");
-    grid2file(du2dx, "../RL_NSE/outputs/du2dx_final.csv");
-    grid2file(duvdy, "../RL_NSE/outputs/duvdy_final.csv");
-    grid2file(d2vdx2, "../RL_NSE/outputs/d2vdx2_final.csv");
-    grid2file(d2vdy2, "../RL_NSE/outputs/d2vdy2_final.csv");
-    grid2file(duvdx, "../RL_NSE/outputs/duvdx_final.csv");
-    grid2file(dv2dy, "../RL_NSE/outputs/dv2dy_final.csv");
-    grid2file(v, "../RL_NSE/outputs/v_final.csv");
+    grid2file(u, "../testing_derivatives/outputs/u.csv");
+    grid2file(d2udx2, "../testing_derivatives/outputs/d2udx2.csv");
+    grid2file(d2udy2, "../testing_derivatives/outputs/d2udy2.csv");
+    grid2file(du2dx, "../testing_derivatives/outputs/du2dx.csv");
+    grid2file(duvdy, "../testing_derivatives/outputs/duvdy.csv");
+    grid2file(d2vdx2, "../testing_derivatives/outputs/d2vdx2.csv");
+    grid2file(d2vdy2, "../testing_derivatives/outputs/d2vdy2.csv");
+    grid2file(duvdx, "../testing_derivatives/outputs/duvdx.csv");
+    grid2file(dv2dy, "../testing_derivatives/outputs/dv2dy.csv");
+    grid2file(v, "../testing_derivatives/outputs/v.csv");
 
 }
