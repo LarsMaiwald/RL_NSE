f948e57877367ee409b9bcee16699bf2d1cabb48
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-06-23 22:59:16 +0200

    making the colorbar range constant

--- streamanimation.py
+++ streamanimation.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import FancyArrowPatch
 from matplotlib.collections import LineCollection
+from matplotlib.colors import Normalize
 from load_cfg import load_cfg
 import os
 
@@ -22,7 +23,7 @@ b = cfg.b
 i_max = cfg.i_max
 j_max = cfg.j_max
 
-counter_max = (len(os.listdir('../RL_NSE/outputs/')) - 1)/3 - 1
+counter_max = int((len(os.listdir('../RL_NSE/outputs/')) - 1)/3 - 1)
 t = np.genfromtxt ('../RL_NSE/outputs/t_final.csv', delimiter=",")
 s = str(t[-1])
 t_dec = len(s[s.rfind('.'):]) - 1
@@ -47,6 +48,21 @@ def load_and_adjust(counter, U, V, P):
             V[i][j] = (v[i][j+1] + v[i+1][j+1])/2
     P = p[1:-1, 1:-1]
     return U, V, P
+
+# finding pressure and velocity min and max
+P_min = 0
+P_max = 0
+speed_min = 0
+speed_max = 0
+for i in range(1, counter_max+1):
+    U, V, P = load_and_adjust(i, U, V, P)
+    speed = np.sqrt(U**2 + V**2)
+    P_min = np.min([P_min, np.min(P)])
+    P_max = np.max([P_max, np.max(P)])
+    speed_min = np.min([speed_min, np.min(speed)])
+    speed_max = np.max([speed_max, np.max(speed)])
+norm_s = Normalize(speed_min, speed_max)
+norm_b = Normalize(P_min, P_max)
     
 U, V, P = load_and_adjust(1, U, V, P)
 
@@ -55,12 +71,14 @@ speed = np.sqrt(U**2 + V**2)
 
 # plotting
 fig, ax = plt.subplots()
-stream = ax.streamplot(X, Y, U, V, color=speed, density=2, cmap='gray')
-background = ax.imshow(P, extent=[0,a,0,b], origin='lower')
+stream = ax.streamplot(X, Y, U, V, color=speed, density=2, cmap='gray', norm=norm_s)
+background = ax.imshow(P, extent=[0,a,0,b], origin='lower', norm=norm_b)
 cbar_s = fig.colorbar(stream.lines, ax=ax, label=r'$\sqrt{u^2 + v^2}$', orientation='vertical', pad=-0.05)
 cbar_b = fig.colorbar(background, ax=ax, label=r'$p$', orientation='vertical', pad=0.13)
 cbar_b.ax.yaxis.set_ticks_position("left")
 cbar_b.ax.yaxis.set_label_position("left")
+cbar_s.formatter.set_powerlimits((0, 0))
+cbar_b.formatter.set_powerlimits((0, 0))
 text = ax.text(1.0, 1.1, f'time: {t[0]}/{t[-1]}', transform=ax.transAxes)
 ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$y$')
@@ -85,12 +103,14 @@ def animation_frame(frame, X, Y, U, V, P, t, a, b):
     U, V, P = load_and_adjust(frame, U, V, P)
     speed = np.sqrt(U**2 + V**2)
     # lw = 5*speed/np.max(speed)
-    stream = ax.streamplot(X, Y, U, V, color=speed, density=2, cmap='gray')
-    background = ax.imshow(P, extent=[0,a,0,b], origin='lower')
+    stream = ax.streamplot(X, Y, U, V, color=speed, density=2, cmap='gray', norm=norm_s)
+    background = ax.imshow(P, extent=[0,a,0,b], origin='lower', norm=norm_b)
     cbar_s = fig.colorbar(stream.lines, ax=ax, label=r'$\sqrt{u^2 + v^2}$', orientation='vertical', pad=-0.05)
     cbar_b = fig.colorbar(background, ax=ax, label=r'$p$', orientation='vertical', pad=0.13)
     cbar_b.ax.yaxis.set_ticks_position("left")
     cbar_b.ax.yaxis.set_label_position("left")
+    cbar_s.formatter.set_powerlimits((0, 0))
+    cbar_b.formatter.set_powerlimits((0, 0))
     text.set_text(f'time: {t[int(frame-1)]:.{t_dec}f}/{t[-1]}')
     return stream, cbar_s, cbar_b, background
 
