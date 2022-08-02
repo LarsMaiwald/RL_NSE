c872e57c1007bbd5d16f8a472e945292f9860def
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-08-02 23:22:46 +0200

    excluding boundary from initialization

--- initialization.cpp
+++ initialization.cpp
@@ -6,8 +6,8 @@ using namespace std;
 // Initialization of grid (a x b) values with predefined example function
 void grid_init_cos_sin(Grid &u, int a, int b)
 {
-    for (int i = 0; i < u.i_max + u.i_g; i++) {
-        for (int j = 0; j < u.j_max + u.j_g; j++) {
+    for (int i = 1; i < u.i_max + u.i_g - 1; i++) {
+        for (int j = 1; j < u.j_max + u.j_g - 1; j++) {
             u.grid[i][j] = cos(i*a/(u.i_max+u.i_g))*sin(j*b/(u.j_max+u.j_g));
         }
     }
@@ -15,8 +15,8 @@ void grid_init_cos_sin(Grid &u, int a, int b)
 
 void grid_init_val(Grid &u, float val)
 {
-    for (int i = 0; i < u.i_max + u.i_g; i++) {
-        for (int j = 0; j < u.j_max + u.j_g; j++) {
+    for (int i = 1; i < u.i_max + u.i_g - 1; i++) {
+        for (int j = 1; j < u.j_max + u.j_g - 1; j++) {
             u.grid[i][j] = val;
         }
     }
