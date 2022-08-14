a31b32419667c3be9956f09ed1d54e84bba35cc1
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-08-14 13:18:10 +0200

    clean up

--- ghia_comparison.py
+++ ghia_comparison.py
@@ -16,24 +16,8 @@ b = 1
 degree = 1
 radius = 0.01
 
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re100_done/outputs*/u_final.csv'
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re400_done/outputs*/u_final.csv'
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re1000_done/outputs*/u_final.csv'
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re3200_done/outputs*/u_final.csv'
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re5000_done/outputs*/u_final.csv'
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re7500_done/outputs*/u_final.csv' # 50
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re7500_again/outputs0/u_final.csv' # 100
-# path_to_file = '../RL_NSE/result_folder_rudi/2022_08_08_01-13_varyingRe/Re10000_done/outputs*/u_final.csv'
-
-path_to_file = '../RL_NSE/results/2022_08_08-16_08-lid_driven_cavity_257_Re100/outputs0/u_final.csv'
-# path_to_file = '../RL_NSE/results/2022_08_09-13_00-lid_driven_cavity_257_Re400/outputs0/u_final.csv'
-# path_to_file = '../RL_NSE/results/2022_08_07-13_48-lid_driven_cavity_257_Re1000/outputs/u_final.csv'
-# path_to_file = '../RL_NSE/results/2022_08_09-13_58-lid_driven_cavity_257_Re3200/outputs0/u_final.csv'
-# path_to_file = '../RL_NSE/results/2022_08_09-14_22-lid_driven_cavity_257_Re5000/outputs1/u_final.csv'
-# path_to_file = '../RL_NSE/results/2022_08_09-16_03-lid_driven_cavity_257_Re7500/outputs0/u_final.csv'
-# path_to_file = '../RL_NSE/results/2022_08_09-10_24-lid_driven_cavity_257_Re10000/outputs1/u_final.csv' # 100
-# path_to_file = '../RL_NSE/results/2022_08_11-20_04-lid_driven_cavity_257_Re10000/outputs1/u_final.csv' # 150
-# path_to_file = '../RL_NSE/results/2022_08_12-14_09-lid_driven_cavity_257_Re10000/outputs0/u_final.csv' # 300
+# choosing u output file to analyze
+path_to_file = '../RL_NSE/results/lid_driven_cavity_257_Re1000/outputs0/u_final.csv'
 
 # loading u array from file
 u = np.genfromtxt(path_to_file, delimiter=',')
